c603839352100e601d76b7c65015f447f24ae69f
Apprentice <apprentice@devbootcamp.com>
2013-09-04 17:53:05 -0700

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: this.method,
+      url: this.action,
+      data:$(this).serialize()
+    }).done(function(server_data){
+      $('#die').html(server_data)
+
+
+    })
+
+
+  })
+
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
